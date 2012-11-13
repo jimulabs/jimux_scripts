53e1949f1384f3e8555c18a220266258f95c8d5e
Linton Ye <lintonye@linton-yes-macbook-2.local>
2012-11-13 10:29:36 -0800

    added launch scripts

---
+++ launch.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# TODO download jimu plugins
+PWD = `pwd`
+ECLIPSE_HOME="`find ${PWD} -name 'eclipse*' -type d | head -1`"
+DISPLAY=:1 $ECLIPSE_HOME/eclipse -consolelog -application jimux.server -data jimux-workspace -clean
