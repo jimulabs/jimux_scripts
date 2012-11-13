09cc792d198008643e6b6b6496a0a5b2a5abb4ce
Linton Ye <lintonye@linton-yes-macbook-2.local>
2012-11-13 10:40:07 -0800

    download launch scripts

--- launch.sh
+++ launch.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# TODO download jimu plugins
+if [ "`ps -ALL | grep Xvfb | wc -l`" -gt "0" ]
+then Xvfb :1 -screen 0 1024x768x24 &
+fi
+
 PWD = `pwd`
 ECLIPSE_HOME="`find ${PWD} -name 'eclipse*' -type d | head -1`"
 DISPLAY=:1 $ECLIPSE_HOME/eclipse -consolelog -application jimux.server -data jimux-workspace -clean
