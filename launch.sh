89b0229a11930a73c7948d92746257b94a2ab695
Linton Ye <lintonye@linton-yes-macbook-2.local>
2012-11-13 11:00:04 -0800

    pwd updates

--- launch.sh
+++ launch.sh
@@ -4,6 +4,6 @@ if [ "`ps -ALL | grep Xvfb | wc -l`" -eq "0" ]
 then Xvfb :1 -screen 0 1024x768x24 &
 fi
 
-PWD = `pwd`
+PWD="`pwd`"
 ECLIPSE_HOME="`find ${PWD} -name 'eclipse*' -type d | head -1`"
 DISPLAY=:1 $ECLIPSE_HOME/eclipse -consolelog -application jimux.server -data jimux-workspace -clean
