c6d5016438c00cdd4a813cdb7c07d5b9e3f55397
Linton Ye <lintonye@linton-yes-macbook-2.local>
2012-11-13 23:08:17 -0800

    set ANDROID_SDK_HOME env var

--- launch_with_osgiconsole.sh
+++ launch_with_osgiconsole.sh
@@ -5,5 +5,6 @@ then Xvfb :1 -screen 0 1024x768x24 &
 fi
 
 PWD="`pwd`"
+export ANDROID_SDK_HOME="`find ${PWD} -name android-sdk* -type d`"
 ECLIPSE_HOME="`find ${PWD} -name 'eclipse*' -type d | head -1`"
 DISPLAY=:1 $ECLIPSE_HOME/eclipse -consolelog -application jimux.server -data jimux-workspace -console -noexit -clean
