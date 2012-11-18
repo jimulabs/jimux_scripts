4950a6b5de2b4eae4a92702889c57ac146f44bf1
Linton Ye <lintonye@linton-yes-macbook-2.local>
2012-11-17 23:00:03 -0800

    added more settings trying to set compilance level to 1.6

--- launch.sh
+++ launch.sh
@@ -31,11 +31,14 @@ EOF
 JDT_UI_PREFS_PATH="${SETTINGS_DIR}/org.eclipse.jdt.ui.prefs"
 echo "Writing JDT_UI prefs: ${JDT_UI_PREFS_PATH}..."
 cat > ${JDT_UI_PREFS_PATH} <<EOF
-eclipse.jdt.core.classpathVariable.JRE_SRC=
-org.eclipse.jdt.core.classpathVariable.JRE_SRCROOT=
+eclipse.preferences.version=1
 org.eclipse.jdt.core.compiler.codegen.inlineJsrBytecode=enabled
 org.eclipse.jdt.core.compiler.codegen.targetPlatform=1.6
+org.eclipse.jdt.core.compiler.codegen.unusedLocal=preserve
 org.eclipse.jdt.core.compiler.compliance=1.6
+org.eclipse.jdt.core.compiler.debug.lineNumber=generate
+org.eclipse.jdt.core.compiler.debug.localVariable=generate
+org.eclipse.jdt.core.compiler.debug.sourceFile=generate
 org.eclipse.jdt.core.compiler.problem.assertIdentifier=error
 org.eclipse.jdt.core.compiler.problem.enumIdentifier=error
 org.eclipse.jdt.core.compiler.source=1.6
