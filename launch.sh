af1cf014eeba42e9dbcd34fdf8dfeabd0c9aa298
Linton Ye <lintonye@linton-yes-macbook-2.local>
2012-11-17 23:17:05 -0800

    use empty workspace

--- launch.sh
+++ launch.sh
@@ -18,7 +18,9 @@ ECLIPSE_HOME="`find ${PWD} -name 'eclipse*' -type d | head -1`"
 WORKSPACE="$HOME/jimux-workspace"
 SETTINGS_DIR="${WORKSPACE}/.metadata/.plugins/org.eclipse.core.runtime/.settings"
 rm -fr $WORKSPACE
-mkdir -p ${SETTINGS_DIR}
+# mkdir -p ${SETTINGS_DIR}
+curl -L -O https://github.com/downloads/jimulabs/jimux_scripts/jimux-workspace.tar.gz
+tar -zxvf jimux-workspace.tar.gz
 
 ADT_PREFS_PATH="${SETTINGS_DIR}/com.android.ide.eclipse.adt.prefs"
 echo "Writing ADT prefs: ${ADT_PREFS_PATH}..."
@@ -28,21 +30,21 @@ com.android.ide.eclipse.adt.sdk=${ANDROID_SDK_HOME}
 eclipse.preferences.version=1
 EOF
 
-JDT_UI_PREFS_PATH="${SETTINGS_DIR}/org.eclipse.jdt.ui.prefs"
-echo "Writing JDT_UI prefs: ${JDT_UI_PREFS_PATH}..."
-cat > ${JDT_UI_PREFS_PATH} <<EOF
-eclipse.preferences.version=1
-org.eclipse.jdt.core.compiler.codegen.inlineJsrBytecode=enabled
-org.eclipse.jdt.core.compiler.codegen.targetPlatform=1.6
-org.eclipse.jdt.core.compiler.codegen.unusedLocal=preserve
-org.eclipse.jdt.core.compiler.compliance=1.6
-org.eclipse.jdt.core.compiler.debug.lineNumber=generate
-org.eclipse.jdt.core.compiler.debug.localVariable=generate
-org.eclipse.jdt.core.compiler.debug.sourceFile=generate
-org.eclipse.jdt.core.compiler.problem.assertIdentifier=error
-org.eclipse.jdt.core.compiler.problem.enumIdentifier=error
-org.eclipse.jdt.core.compiler.source=1.6
-EOF
+# JDT_UI_PREFS_PATH="${SETTINGS_DIR}/org.eclipse.jdt.ui.prefs"
+# echo "Writing JDT_UI prefs: ${JDT_UI_PREFS_PATH}..."
+# cat > ${JDT_UI_PREFS_PATH} <<EOF
+# eclipse.preferences.version=1
+# org.eclipse.jdt.core.compiler.codegen.inlineJsrBytecode=enabled
+# org.eclipse.jdt.core.compiler.codegen.targetPlatform=1.6
+# org.eclipse.jdt.core.compiler.codegen.unusedLocal=preserve
+# org.eclipse.jdt.core.compiler.compliance=1.6
+# org.eclipse.jdt.core.compiler.debug.lineNumber=generate
+# org.eclipse.jdt.core.compiler.debug.localVariable=generate
+# org.eclipse.jdt.core.compiler.debug.sourceFile=generate
+# org.eclipse.jdt.core.compiler.problem.assertIdentifier=error
+# org.eclipse.jdt.core.compiler.problem.enumIdentifier=error
+# org.eclipse.jdt.core.compiler.source=1.6
+# EOF
 
 
 
